db8819937dd9b02241996222cee65b1fa31ef49f
I_owned_you <mrkgitonga@gmail.com>
2025-04-25 10:57:02 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,13 @@
+#This is the Database connection setup
+#This is like a bridge It will connect both endss
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./health.db"  # SQLite file-based DB
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
+
